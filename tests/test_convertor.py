3edb961f1af8787f0eb57fadc6c7a0740fc9422d
pengfei liu <liu.pengfei@hotmail.fr>
2026-05-06 10:09:58 +0200

    update unit test

--- tests/test_convertor.py
+++ tests/test_convertor.py
@@ -2,15 +2,15 @@ from krb.convertor import convert_kirbi
 
 
 def test_convertor_with_given_path():
-    kirbi = "C:/Users/pliu/Documents/git/krbTicketConvertor/pkrbconverter/tests/tmp/tgt.kirbi"
-    ccache = "C:/Users/pliu/Documents/git/krbTicketConvertor/pkrbconverter/tests/tmp/tgt.ccache"
+    kirbi = "C:/Users/pliu/Documents/git/krbTicketConvertor/tests/tmp/tgt.kirbi"
+    ccache = "C:/Users/pliu/Documents/git/krbTicketConvertor/tests/tmp/tgt.ccache"
 
     convert_kirbi(kirbi, ccache)
 
 def test_convertor_with_default_path():
-    kirbi = "C:/Users/pliu/Documents/git/krbTicketConvertor/pkrbconverter/tests/tmp/tgt.kirbi"
+    kirbi = "C:/Users/pliu/Documents/git/krbTicketConvertor/tests/tmp/tgt.kirbi"
     convert_kirbi(kirbi)
 
 def test_convertor_with_relative_path():
-    kirbi = "tests/tmp/tgt.kirbi"
+    kirbi = "./tests/tmp/tgt.kirbi"
     convert_kirbi(kirbi)
